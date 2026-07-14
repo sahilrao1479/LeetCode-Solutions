a9db991d9b655ded959e8bb77ae218be116ab68f
Sahil Rao <sahilrao031985@gmail.com>
2026-07-14 12:47:41 +0530

    Add LeetCode 21 - Merge Two Sorted Lists

---
+++ LinkedList/21-Merge-Two-Sorted-Lists.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode prev= new ListNode(-1);
+        ListNode curr = prev;
+
+        while (list1!=null && list2!=null){
+            if (list1.val<=list2.val){
+                prev.next=list1;
+                list1=list1.next;
+            }
+            else {
+                prev.next=list2;
+                list2=list2.next;
+            }
+            prev=prev.next;
+
+        }
+            if (list1!=null){
+                prev.next=list1;
+            }
+             if (list2!=null){
+                prev.next=list2;
+            }
+        
+        return curr.next;
+    }
+}
