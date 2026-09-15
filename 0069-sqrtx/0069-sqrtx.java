a5471ad52c0398a39c36ee6a88081b9d10ca97b9
Sahil Rao <sahilrao031985@gmail.com>
2026-09-15 23:31:00 +0530

    Time: 1 ms (99.23%), Space: 42.8 MB (23.7%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -5,7 +5,7 @@ class Solution {
 
         int start = 1;
         int end = x;
-        int mid = -1;
+        int mid = 1;
 
         while (start <= end) {
             mid = start + (end - start) / 2;
